1988b79bc76ba4f200ac4ba186c8cecc9a382f26
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2012-10-30 23:18:41 +0100

    Début de l'implémentation des feeds
    	implementation dans la base de données
    	début d'implémentation du feed Astronomy Picture of the Day
    	fonctions de support variées

---
+++ lib/db.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once ('../config.php');
+
+$db = null;
+
+function _db_connect ($user, $pass) {
+	GLOBAL $DBHOST,$DBPORT,$DBNAME,$db;
+	
+	if ($db===null) {
+		$connstr = "host=$DBHOST port=$DBPORT dbname=$DBNAME user=$user password=$pass";
+		$db = pg_connect($connstr);
+	}
+	return $db;
+}
+
+function db_connect () {
+	GLOBAL $DBUSER,$DBPASS;
+	return _db_connect($DBUSER,$DBPASS);
+}
+
+function db_query ($s, $p=null) {
+	global $db;
+	if ($p==null) 
+		$p = array();
+	return pg_query_params ($db, $s, $p);
+}
+
+function db_num_rows ($r) {
+	return pg_num_rows($r);
+}
+
+function db_fetch_assoc ($r) {
+	return pg_fetch_assoc($r);
+}
+
+?>
--- sql/signage.sql
+++ sql/signage.sql
@@ -19,7 +19,10 @@ do $$
 			raise notice 'No version found, creating database';
 
 			-- create a user
-			create user 'signage' with encryped password 'eiph8Ahg';
+			perform  usename from pg_user where usename='signage';
+			if not found then
+				create user signage with encrypted password 'eiph8Ahg';
+			end if;
 
 			-- version table.
 			raise notice 'Creating version table';
@@ -40,15 +43,19 @@ do $$
 			alter table feed_types add primary key using index pk_feed_types__id;
 			create unique index ix_feed_types__name on feed_types(name);
 
+			grant select on feed_types to signage;
+
 			insert into feed_types (name) values 
 				('rss'), ('apod');
 
-			-- feeds themselves
+			-- feeds
+			-- cached : defines if the feed contents is to be saved 
 			raise notice 'Creating feeds';
 			create table feeds (
 				id		bigint not null,
 				id_type		bigint not null,
-				url		text
+				url		text,
+				cached		boolean default true
 			);
 			create sequence seq_feeds;
 			alter table feeds alter column id set default nextval('seq_feeds');
@@ -60,6 +67,25 @@ do $$
 			select id into apod_id from feed_types where name='apod';
 			insert into feeds (id_type, url) values
 				(apod_id, 'http://apod.nasa.gov/apod/archivepix.html');
+			
+			grant select on feeds to signage;
+
+			create table feed_contents (
+				id_feed		bigint not null,
+				date		timestamp,
+				title		text,	
+				image		text,
+				detail		text
+			);		
+			create unique index pk_feed_contents on feed_contents(id_feed, date);
+			alter table feed_contents add primary key using index pk_feed_contents;
+			create index fk_feed_contents__id_feed on feed_contents(id_feed);
+			alter table feed_contents add foreign key (id_feed) references feeds(id);
+
+			grant select, insert on feed_contents to signage;
+
+			insert into feed_contents (id_feed,date, title, image,detail) values (1,'2012-10-29','test1','url1','detail1');
+			insert into feed_contents (id_feed,date, title, image,detail) values (1,'2012-10-30','test2','url2','detail2');
 
 		else
 			raise notice 'Database already setup, skipping';
