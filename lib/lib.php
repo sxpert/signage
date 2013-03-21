144562f91bfdb7bd560a7ecae69e599ce64417e8
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2013-03-21 16:00:16 +0100

    ajoutes une option au curl pour fail si 40x

--- lib/lib.php
+++ lib/lib.php
@@ -59,6 +59,7 @@ function get_url_contents($url) {
 		}
 	}
 	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_FAILONERROR, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$data = curl_exec($ch);
 	curl_close($ch);
