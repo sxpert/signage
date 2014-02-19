99f3625451751bf5361322bf924899ce576c829f
Raphaël Jacquot <raphael.jacquot@obs.ujf-grenoble.fr>
2014-02-19 14:32:37 +0100

    add more options to the default config

--- config.dist.php
+++ config.dist.php
@@ -8,4 +8,14 @@ $DBNAME='signage';
 $DBUSER='user';
 $DBPASS='password';
 
+$HTTP_OPTS=array(
+     'timeout'=>5,
+     'proxy'=>'tcp://address:port/',
+);
+
+$LDAP_SRV='ldap.example.org';
+$LDAP_PORT=389;
+$LDAP_BASEDN='dc=example,dc=org';
+
+
 ?>
